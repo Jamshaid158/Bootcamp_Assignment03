c365d7f77a87180791955b97b4a62ab3884babd2
Jamshaid Zafar <58720440+Jamshaid158@users.noreply.github.com>
2022-10-11 06:39:24 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    //Making a class named as Candidate
+    class Candidate
+    { 
+        //Declaring member variables for the basic info of candidates
+        public string name;
+        public string CNIC;
+        public string CGPA;
+        public string city;
+    }
+
+
+    ////Making a class named as Document for candidates documents
+    //class Document
+    //{
+    //    //Declaring member variables for the documents
+    //    public string fileName;
+    //    public string filePath;
+
+    //}
+}
